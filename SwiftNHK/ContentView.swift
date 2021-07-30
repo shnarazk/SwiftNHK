207cb640ae12f07043cd8ea4573291f80e9ee649
shujinarazaki <shujinarazaki@protonmail.com>
2021-07-30 13:52:33 +0900

    works at bottom line

--- SwiftNHK/ContentView.swift
+++ SwiftNHK/ContentView.swift
@@ -9,12 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var secretKey: String = ""
-    @State var secretId: String = ""
-    var cp: CurrentProgram? {
-        get {
-            load_data(area: "400", service: "g1", apiKey: secretKey)
-        }
-    }
+    @State var cp: CurrentProgram?
     var body: some View {
         VStack {
             List {
@@ -22,8 +17,16 @@ struct ContentView: View {
                     ForEach([p.previous, p.present, p.following]) { p in
                         HStack {
                             Text(p.start_time)
+                            AsyncImage(url: URL(string: "https:" + p.service.logo_s.url)) {
+                                image in image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 64, height: 64)
+                            .clipped()
                             VStack {
                                 Text(p.title)
+                                    .padding()
                                 Text(p.subtitle)
                             }
                         }
@@ -32,10 +35,21 @@ struct ContentView: View {
                     Text("fail to load program")
                 }
             }
-            NavigationView() {
-                NavigationLink(destination: SecretsPanel(secretKey: $secretKey, secretId: $secretId)) {
-                    Text("Set secrets")
+            .task {
+                cp = load_data(area: "400", service: "g1", apiKey: secretKey)
+            }
+            .refreshable {
+                cp = load_data(area: "400", service: "g1", apiKey: secretKey)
+            }
+            HStack {
+                Text("Key")
+                    .padding()
+                TextField("Key", text: $secretKey)
+                    .padding()
+                Button("Refresh") {
+                    cp = load_data(area: "400", service: "g1", apiKey: secretKey)
                 }
+                .padding()
             }
         }
     }
@@ -43,21 +57,9 @@ struct ContentView: View {
 
 struct SecretsPanel: View {
     @Binding var secretKey: String
-    @Binding var secretId: String
     var body: some View {
         VStack {
-            HStack {
-                Text("Sectet")
-                    .padding()
-                TextField("ID", text: $secretId)
-                    .padding()
-            }
-            HStack {
-                Text("KeyId")
-                    .padding()
-                TextField("Key", text: $secretKey)
-                    .padding()
-            }
+
         }
     }
 }
