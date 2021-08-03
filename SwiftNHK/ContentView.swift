d79b50ba12aee22248945b168afb9b8ef24c2294
shujinarazaki <shujinarazaki@protonmail.com>
2021-08-03 20:00:27 +0900

    use environment variables

--- SwiftNHK/ContentView.swift
+++ SwiftNHK/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var secretKey: String = ""
+    @State var apiKey: String = ProcessInfo.processInfo.environment["APIKey"] ?? ""
     @State var cp: CurrentProgram?
     var body: some View {
         VStack {
@@ -17,13 +17,14 @@ struct ContentView: View {
                     ForEach([p.previous, p.present, p.following]) { p in
                         HStack {
                             Text(p.start_time)
-                            AsyncImage(url: URL(string: "https:" + p.service.logo_s.url)) {
-                                image in image.resizable()
+                            AsyncImage(url: URL(string: "https:" + p.service.logo_s.url)) { image in image.resizable()
+                                    .frame(width: 40, height: 40)
+                                    .clipped()
                             } placeholder: {
                                 ProgressView()
                             }
-                            .frame(width: 64, height: 64)
-                            .clipped()
+                            // .frame(width: 64, height: 64)
+                            // .clipped()
                             VStack {
                                 Text(p.title)
                                     .padding()
@@ -36,18 +37,18 @@ struct ContentView: View {
                 }
             }
             .task {
-                cp = load_data(area: "400", service: "g1", apiKey: secretKey)
+                cp = load_data(area: "400", service: "g1", apiKey: apiKey)
             }
             .refreshable {
-                cp = load_data(area: "400", service: "g1", apiKey: secretKey)
+                cp = load_data(area: "400", service: "g1", apiKey: apiKey)
             }
             HStack {
                 Text("Key")
                     .padding()
-                TextField("Key", text: $secretKey)
+                TextField("Key", text: $apiKey)
                     .padding()
                 Button("Refresh") {
-                    cp = load_data(area: "400", service: "g1", apiKey: secretKey)
+                    cp = load_data(area: "400", service: "g1", apiKey: apiKey)
                 }
                 .padding()
             }
