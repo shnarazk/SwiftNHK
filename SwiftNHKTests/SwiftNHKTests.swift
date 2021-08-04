84472d06f323a25ac180792cd84499e795d67360
shujinarazaki <shujinarazaki@protonmail.com>
2021-08-05 08:27:24 +0900

    Add a simple test

--- SwiftNHKTests/SwiftNHKTests.swift
+++ SwiftNHKTests/SwiftNHKTests.swift
@@ -6,9 +6,10 @@
 //
 
 import XCTest
+@testable import SwiftNHK
 
 class SwiftNHKTests: XCTestCase {
-
+    var apiKey: String = ProcessInfo.processInfo.environment["APIKey"] ?? ""
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -20,6 +21,7 @@ class SwiftNHKTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(load_data(area: "400", service: "g1", apiKey: apiKey) != nil)
     }
 
     func testPerformanceExample() throws {
