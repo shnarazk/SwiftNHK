275113331a60b4a66e306c3994cb62296e9b0390
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-06-13 01:23:41 +0900

    Switch to Swift 6.2

--- SwiftNHK/ContentView.swift
+++ SwiftNHK/ContentView.swift
@@ -16,54 +16,57 @@ struct ContentView: View {
         }
     }
     var body: some View {
-        VStack {
-            List {
-                if let p = cp {
-                    ForEach([p.previous, p.present, p.following]) { p in
-                        HStack {
-                            Text(p.start_time)
-                            AsyncImage(url: URL(string: "https:" + p.service.logo_s.url)) { image in image.resizable()
-                                    .frame(width: 40, height: 40)
-                                    .clipped()
-                            } placeholder: {
-                                ProgressView()
-                            }
-                            // .frame(width: 64, height: 64)
-                            // .clipped()
-                            VStack {
-                                Text(p.title)
-                                    .padding()
-                                Text(p.subtitle)
+        NavigationSplitView {}
+        detail: {
+            VStack {
+                List {
+                    if let p = cp {
+                        ForEach([p.previous, p.present, p.following]) { p in
+                            HStack {
+                                Text(p.start_time)
+                                AsyncImage(url: URL(string: "https:" + p.service.logo_s.url)) { image in image.resizable()
+                                        .frame(width: 40, height: 40)
+                                        .clipped()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                // .frame(width: 64, height: 64)
+                                // .clipped()
+                                VStack {
+                                    Text(p.title)
+                                        .padding()
+                                    Text(p.subtitle)
+                                }
                             }
                         }
+                    } else {
+                        Text("fail to load program")
                     }
-                } else {
-                    Text("fail to load program")
                 }
-            }
 
-            .refreshable {
-                channel = "g1"
-            }
-            Picker(selection: $channel, label: EmptyView(), content: {
-                Text("総合").tag("g1")
-                Text("Eテレ").tag("e1")
-                Text("Eテレサブチャンネル").tag("e2")
-                Text("NHK radio1").tag("r1")
-                Text("NHK radio2").tag("r2")
-                Text("NHK FM").tag("r3")
-            })
-            .pickerStyle(SegmentedPickerStyle())
-            .padding(.horizontal, 10)
-            HStack {
-                Text("Key")
-                    .padding()
-                TextField("Key", text: $apiKey)
-                    .padding()
-                Button("Refresh") {
+                .refreshable {
                     channel = "g1"
                 }
-                .padding()
+                Picker(selection: $channel, label: EmptyView(), content: {
+                    Text("総合").tag("g1")
+                    Text("Eテレ").tag("e1")
+                    Text("Eテレサブチャンネル").tag("e2")
+                    Text("NHK radio1").tag("r1")
+                    Text("NHK radio2").tag("r2")
+                    Text("NHK FM").tag("r3")
+                })
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(.horizontal, 10)
+                HStack {
+                    Text("Key")
+                        .padding()
+                    TextField("Key", text: $apiKey)
+                        .padding()
+                    Button("Refresh") {
+                        channel = "g1"
+                    }
+                    .padding()
+                }
             }
         }
     }
