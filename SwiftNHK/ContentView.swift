cc265303a559a05d6b6073008cfb1f6c5cb55f0d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-06-13 02:01:35 +0900

    misc changes

--- SwiftNHK/ContentView.swift
+++ SwiftNHK/ContentView.swift
@@ -11,32 +11,91 @@ struct ContentView: View {
     @State var apiKey: String = ProcessInfo.processInfo.environment["APIKey"] ?? ""
     @State var channel: String
     var cp: CurrentProgram? {
-        get {
-            load_data(area: "400", service: channel, apiKey: apiKey)
-        }
+        load_data(area: "400", service: channel, apiKey: apiKey)
     }
     var body: some View {
-        NavigationSplitView {}
-        detail: {
+        NavigationSplitView {
+            List {
+                Button(action: { channel = "g1" }) {
+                    HStack {
+                        Text("総合")
+                        if channel == "g1" {
+                            Spacer()
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+                Button(action: { channel = "e1" }) {
+                    HStack {
+                        Text("Eテレ")
+                        if channel == "e1" {
+                            Spacer()
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+                Button(action: { channel = "e2" }) {
+                    HStack {
+                        Text("Eテレサブチャンネル")
+                        if channel == "e2" {
+                            Spacer()
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+                Button(action: { channel = "r1" }) {
+                    HStack {
+                        Text("NHK radio1")
+                        if channel == "r1" {
+                            Spacer()
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+                Button(action: { channel = "r2" }) {
+                    HStack {
+                        Text("NHK radio2")
+                        if channel == "r2" {
+                            Spacer()
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+                Button(action: { channel = "r3" }) {
+                    HStack {
+                        Text("NHK FM")
+                        if channel == "r3" {
+                            Spacer()
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+            }
+        } detail: {
             VStack {
                 List {
                     if let p = cp {
                         ForEach([p.previous, p.present, p.following]) { p in
-                            HStack {
-                                Text(p.start_time)
-                                AsyncImage(url: URL(string: "https:" + p.service.logo_s.url)) { image in image.resizable()
-                                        .frame(width: 40, height: 40)
-                                        .clipped()
-                                } placeholder: {
-                                    ProgressView()
-                                }
+                            VStack(alignment: .leading) {
+                                //                                AsyncImage(url: URL(string: "https:" + p.service.logo_s.url)) { image in image.resizable()
+                                //                                        .frame(width: 40, height: 40)
+                                //                                        .clipped()
+                                //                                } placeholder: {
+                                //                                    ProgressView()
+                                //                                }
                                 // .frame(width: 64, height: 64)
                                 // .clipped()
-                                VStack {
-                                    Text(p.title)
-                                        .padding()
-                                    Text(p.subtitle)
-                                }
+                                Text(p.title)
+                                    .font(.title)
+                                    // .font(.headline)
+                                    // .font(.largeTitle)
+
+                                Text(p.start_time)
+                                    .padding(.leading)
+                                Text(p.subtitle)
+                                    // .font(.headline)
+                                    .padding()
+
                             }
                         }
                     } else {
@@ -47,28 +106,30 @@ struct ContentView: View {
                 .refreshable {
                     channel = "g1"
                 }
-                Picker(selection: $channel, label: EmptyView(), content: {
-                    Text("総合").tag("g1")
-                    Text("Eテレ").tag("e1")
-                    Text("Eテレサブチャンネル").tag("e2")
-                    Text("NHK radio1").tag("r1")
-                    Text("NHK radio2").tag("r2")
-                    Text("NHK FM").tag("r3")
-                })
-                .pickerStyle(SegmentedPickerStyle())
-                .padding(.horizontal, 10)
-                HStack {
-                    Text("Key")
-                        .padding()
-                    TextField("Key", text: $apiKey)
-                        .padding()
-                    Button("Refresh") {
-                        channel = "g1"
-                    }
-                    .padding()
-                }
+                //                Picker(selection: $channel, label: EmptyView(), content: {
+                //                    Text("総合").tag("g1")
+                //                    Text("Eテレ").tag("e1")
+                //                    Text("Eテレサブチャンネル").tag("e2")
+                //                    Text("NHK radio1").tag("r1")
+                //                    Text("NHK radio2").tag("r2")
+                //                    Text("NHK FM").tag("r3")
+                //                })
+                //                .pickerStyle(SegmentedPickerStyle())
+                //                .padding(.horizontal, 10)
+
+                //                HStack {
+                //                    Text("Key")
+                //                        .padding()
+                //                    TextField("Key", text: $apiKey)
+                //                        .padding()
+                //                    Button("Refresh") {
+                //                        channel = "g1"
+                //                    }
+                //                    .padding()
+                //                }
             }
         }
+        // .backgroundExtensionEffect()
     }
 }
 
